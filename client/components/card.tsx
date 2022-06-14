90f0995f6e2ccdcb4a0ddefd41ea1e06c7850a35
DmitriyBakhanenko <bahanenko@gmail.com>
2022-06-14 14:49:30 +0300

    card comp fixes

--- client/components/card.tsx
+++ client/components/card.tsx
@@ -1,22 +1,15 @@
 import { GridItem, Flex, Avatar, Heading, Text } from "@chakra-ui/react";
+import { Faker } from "./facker"
 
-interface Client {
-  data: {
-    avatar: string,
-    firstName: string,
-    lastName: string,
-    text: string
-  }
-}
 
-const Card: React.FC<Client> = ({ data: { avatar, firstName, lastName, text } }) => (
+const Card: React.FC = () => (
   <GridItem w='300px' h='250px' bg='whiteAlpha.200' display='flex' flexDir='column' p={6}>
     <Flex>
-      <Avatar mr={4} src={avatar} />
+      <Avatar mr={4} src={Faker.getAvatar()} />
       <Heading color='white' alignSelf='center'
-        as='h2' fontSize={20}>{firstName + '.' + lastName}</Heading>
+        as='h2' fontSize={20}>{Faker.getFirstName() + '.' + Faker.getLastName()}</Heading>
     </Flex>
-    <Text color='whiteAlpha.700' mt={4}>{text}</Text>
+    <Text color='whiteAlpha.700' mt={4}>{Faker.getText()}</Text>
   </GridItem>
 )
 
