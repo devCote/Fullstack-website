761dc3eb6d92c26a5bc888c39565910d0805c082
DmitriyBakhanenko <bahanenko@gmail.com>
2022-06-14 19:36:26 +0300

    static route

--- server/index.js
+++ server/index.js
@@ -5,6 +5,7 @@ const cors = require('cors')
 const { apiRouter, serverRouter } = require('./routes/index')
 const fileUpload = require('express-fileupload')
 const fs = require('fs')
+const path = require('path')
 
 const PORT = process.env.PORT || 5000
 const app = express()
@@ -12,6 +13,7 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 app.use(fileUpload({}))
+app.use(express.static(path.resolve(__dirname, 'static')))
 app.use('/api', apiRouter)
 app.use('/server', serverRouter)
 
