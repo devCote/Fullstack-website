7e6c54ad05ffb8def28b09f2f0759947d21394be
DmitriyBakhanenko <bahanenko@gmail.com>
2022-06-15 16:03:53 +0300

    svg to image change

--- client/components/card.tsx
+++ client/components/card.tsx
@@ -1,4 +1,4 @@
-import { GridItem, Flex, Avatar, Heading, Text } from "@chakra-ui/react";
+import { GridItem, Flex, Box, Heading, Text, Image } from "@chakra-ui/react";
 
 type user = {
   user: {
@@ -11,14 +11,23 @@ type user = {
 
 const Card = ({ user }: user) => {
 
+  console.log(user.image)
+
   return (
     <GridItem w='300px' h='250px' bg='whiteAlpha.200' display='flex' flexDir='column' p={6}>
       <Flex>
-        <Avatar mr={4} src={user.image} />
+        <Image borderColor={"whiteAlpha.800"}
+          objectFit="cover"
+          borderWidth={2} borderStyle='solid'
+          width='50px' height='50px'
+          display="inline-block" borderRadius='full'
+          alt={user.image} mr={4} src={user.image}
+        />
         <Heading color='white' alignSelf='center' as='h2' fontSize={20}>
           {user.firstName + '.' + user.lastName}
         </Heading>
       </Flex>
+
       <Text color='whiteAlpha.700' mt={4}>{user.text}</Text>
     </GridItem>
   )
