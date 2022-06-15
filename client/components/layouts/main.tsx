ba760a3a3b5f75bd1b1b34b2f978e0c0e105ed4a
DmitriyBakhanenko <bahanenko@gmail.com>
2022-06-15 16:03:38 +0300

    fixin main comp

--- client/components/layouts/main.tsx
+++ client/components/layouts/main.tsx
@@ -7,16 +7,18 @@ const Main = ({ children }: any) => {
   return (
     <Box as='main'>
       <Head>
-        <meta name="description" content="..." />
-        <meta name="keywords" content="..." />
+        <meta name="description" content="night city life" />
+        <meta name="keywords" content="crawlers, night city, night" />
         <meta name="author" content="Dmitriy Bakhanenko" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="lang" content="en" />
       </Head>
       <Navbar />
       <Image
         src="/back.jpg"
         alt="background img"
         position='absolute'
+        objectFit='cover'
         zIndex={-1}
         width='100%'
         height='90vh'
